2bd6cc6a1c904ecfca786ee00342456fe5bee3bd
Sebastian Wilzbach <sebi@wilzbach.me>
2014-12-08 23:05:25 +0100

    avoid the default keyword (IE8)

--- src/schemeclass.js
+++ src/schemeclass.js
@@ -1,28 +1,27 @@
 var StaticSchemeClass = function(map){
-  this.default = "#ffffff";
+  this.defaultColor = "#ffffff";
   this.type = "static";
   this.map = map;
   this.getColor = function(letter){
-    if(this.map[letter] != undefined){
+    if(this.map[letter] !== undefined){
       return this.map[letter]; 
     }else{
-      return this.default;
+      return this.defaultColor;
     }
-  }
-}
+  };
+};
 
 var DynSchemeClass = function(fun,opt){
-  this.default = "#ffffff";
   this.type = "dyn";
   this.opt = opt;
   // init
-  if(fun.init != undefined){
+  if(fun.init !== undefined){
     fun.init.call(this);
     this.getColor = fun.run;
     this.reset = fun.init;
   }else{
     this.getColor = fun;
   }
-}
+};
 module.exports.stat = StaticSchemeClass;
 module.exports.dyn = DynSchemeClass;
